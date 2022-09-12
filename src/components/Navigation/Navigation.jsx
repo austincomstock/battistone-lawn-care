b66e6208ec2a959974df66ffdeb68f0b3d1d56d4
austincomstock <austinjcomstock@gmail.com>
2022-09-12 16:59:52 -0600

    Added location list item & href

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,14 +10,9 @@ export const Navigation = () => {
     <div id="Nav1">
       <ul id="Nav2">
         <li>
-          <GoLocation /> West Haven, Utah
-        </li>
-        <li>
           <BsTelephone /> 385-626-9489
         </li>
-        <li>
-          <AiOutlineMail /> battistonelawncare@gmail.com
-        </li>
+
         <li>
           <a
             className="A1"
@@ -37,6 +32,19 @@ export const Navigation = () => {
           >
             <BsInstagram /> Instagram
           </a>
+        </li>
+        <li>
+          <a
+            className="A1"
+            href="https://www.google.com/maps/place/West+Haven,+UT/@41.2096563,-112.1196149,12z/data=!3m1!4b1!4m5!3m4!1s0x87530ed77073d993:0x979ce0cd87d5794!8m2!3d41.2029998!4d-112.0510533"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GoLocation /> West Haven, Utah
+          </a>
+        </li>
+        <li>
+          <AiOutlineMail /> battistonelawncare@gmail.com
         </li>
       </ul>
     </div>
