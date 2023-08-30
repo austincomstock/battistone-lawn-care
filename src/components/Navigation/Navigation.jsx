bb8bc2287b6f017f0ea92f4d62d97992db3b7507
Austin Comstock <austinjcomstock@gmail.com>
2023-08-30 09:56:16 -0600

    Updated location to Weber County

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -36,11 +36,11 @@ export const Navigation = () => {
         <li>
           <a
             className="A1"
-            href="https://www.google.com/maps/place/West+Haven,+UT/@41.2096563,-112.1196149,12z/data=!3m1!4b1!4m5!3m4!1s0x87530ed77073d993:0x979ce0cd87d5794!8m2!3d41.2029998!4d-112.0510533"
+            href="https://www.google.com/maps/place/Weber+County,+UT/@41.2649447,-112.1560139,10.5z/data=!4m6!3m5!1s0x87530c0b357d8a97:0x6305f2940ccb94f4!8m2!3d41.2602635!4d-111.9522491!16zL20vMGpjbWo?entry=ttu"
             target="_blank"
             rel="noreferrer"
           >
-            <GoLocation /> West Haven, Utah
+            <GoLocation /> Weber County, Utah
           </a>
         </li>
         <li>
